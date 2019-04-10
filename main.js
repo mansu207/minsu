60b877dc7796fba4f38c9b52cf10a48bc6149d2f
KimMinSu <kim@minsu.kr>
2019-04-10 14:12:04 +0900

    test3

---
+++ main.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+
+    var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    console.log(ip)
+ 
+});
+
+app.listen(80);
